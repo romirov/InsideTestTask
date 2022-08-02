4be442df850217dfbb76270f053ff8825e658c59
hanza <romirov@gmail.com>
2022-08-02 22:07:33 +0300

    Created migration file for flyway

---
+++ src/main/resources/db/migration/V1__Creating_user_and_message_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS user_table (
+    id SERIAL,
+    username VARCHAR(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    PRIMARY KEY(id, username),
+    UNIQUE(username)
+);
+
+CREATE TABLE IF NOT EXISTS message_table (
+    id SERIAL,
+    username VARCHAR(100) NOT NULL,
+    message VARCHAR(100),
+    PRIMARY KEY(id, username),
+    FOREIGN KEY (username) REFERENCES user_table(username) ON DELETE CASCADE 
+);
